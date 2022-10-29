43a768b27058c459486db61fd8a572af4552053b
gavin7681 <pr.govind81@gmail.com>
2022-10-28 22:20:56 -0400

    commit with new change

--- src/main/java/com/leafBot/testcases/TC008_TestCaseExample.java
+++ src/main/java/com/leafBot/testcases/TC008_TestCaseExample.java
@@ -3,6 +3,7 @@ package com.leafBot.testcases;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +13,10 @@ public class TC008_TestCaseExample {
 	@Test
 	public void example() {
 		WebDriverManager.chromedriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("headless");
+		opt.addArguments("disable.gp");
+		ChromeDriver driver = new ChromeDriver(opt);
 		driver.get("https://leafground.com");
 
 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
