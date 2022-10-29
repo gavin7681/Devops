1d4089456ca3e33e22dc8b1fd336089f651fc82e
gavin7681 <pr.govind81@gmail.com>
2022-10-29 06:08:39 -0400

    chrome options inserted

--- src/main/java/com/leafBot/selenium/api/base/SeleniumBase.java
+++ src/main/java/com/leafBot/selenium/api/base/SeleniumBase.java
@@ -19,6 +19,7 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -259,6 +260,8 @@ public class SeleniumBase extends Reporter implements Browser, Element{
 		try {
 			if(browser.equalsIgnoreCase("chrome")) {
 				WebDriverManager.chromedriver().setup();
+				ChromeOptions opt = new ChromeOptions();
+				opt.addArguments("headless");
 				driver = new ChromeDriver();
 			} else if(browser.equalsIgnoreCase("firefox")) {
 				WebDriverManager.firefoxdriver().setup();
